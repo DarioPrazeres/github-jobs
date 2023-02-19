30c413a7f153b27ef1f5eaeaea266bce0bb943f4
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-02-19 20:02:12 +0100

    update files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import useFetch from './component/useFetch';
 
 function App(){
---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header>
+            <h1>GitHub Jobs</h1>
+        </header>
+    );
+}
