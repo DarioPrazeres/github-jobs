c57752374f0f6424ada9f7eba2e0291bf5e56124
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-02-24 23:38:19 +0100

    update control

--- src/component/Control.js
+++ src/component/Control.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { useContext} from "react";
+import { useContext } from "react";
 import { DataContext } from "../App";
 import leftIcon from "../img/left.png";
 import rightIcon from "../img/right.png";
 function Control() {
-    const { data, number} = useContext(DataContext);
+    const { data, number } = useContext(DataContext);
+    var count = 0;
     console.log("FIRST NUMBER", number)
     return (
         <div className="control">
             <Seta seta={leftIcon} side="left" />
             {
                 data && data.jobs.map((item, index) => {
-                    if(index === 0){
-                        return <Position sp='active' number={index + 1} key={index + 1} />
-                    }else{
-                        return <Position number={index + 1} key={index + 1} />
+                    if (index < 10) {
+                        if (index === 0) {
+                            return <Position sp='active' number={index + 1} key={index + 1} />
+                        } else {
+                            return <Position number={index + 1} key={index + 1} />
+                        }
                     }
-                    
+
                 })
             }
             <Seta seta={rightIcon} side="right" />
@@ -25,7 +28,7 @@ function Control() {
 }
 function Position(props) {
     return (
-        <div className={`seta ${props.sp}`}   id={props.number}>
+        <div className={`seta ${props.sp}`} id={props.number}>
             {props.number}
         </div>
     )
@@ -47,14 +50,14 @@ function updateSeta(side, number) {
     console.log("NUMBER", number);
     console.log("SIDE", side)
     var update = number;
-    if (side === 'right' && number >= 1 && number < 100) {
+    if (side === 'right' && number >= 1 && number < 11) {
         update = number + 1;
-        document.getElementById(update+1).classList.remove('active');
-        document.getElementById(update-1).classList.remove('active');
+        document.getElementById(update + 1).classList.remove('active');
+        document.getElementById(update - 1).classList.remove('active');
         document.getElementById(update).classList.add('active');
     } else if (side === 'left' && number > 1) {
         update = number - 1;
-        document.getElementById(update+1).classList.remove('active');
+        document.getElementById(update + 1).classList.remove('active');
         document.getElementById(update).classList.add('active');
     }
 
