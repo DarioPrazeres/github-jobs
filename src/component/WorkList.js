5cd7398476887826752024c5f645609bee99d41f
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-03-02 21:26:09 +0100

    update full time section

--- src/component/WorkList.js
+++ src/component/WorkList.js
@@ -3,55 +3,46 @@ import Vacancy from "./Vacancy";
 import { useContext } from "react";
 import { DataContext } from "../App";
 function WorkList() {
-    var cont=0;   
     const { data, number, status } = useContext(DataContext);
-    var length = lengthArray(data)
+    var length = lengthArray(data, status)
     console.log('LENGTH FUL time', length);
     return (
         <div className="worklist">
-            {data &&
-                data.jobs.map((item, index) => {
-                    if (status === true && item.job_type === 'full_time') {
-                        cont++;
-                        console.log(cont)
-                        return (<Vacancy
-                            key={item.id}
-                            nameEnterprise={item.company_name}
-                            logo={item.company_logo}
-                            namePosition={item.title}
-                            status={item.job_type || 'NOT FOUND'}
-                            cityName={item.candidate_required_location || 'WorldWide'
-                            }
-                            day={daysAgo(item.publication_date)}
-                        />);
-                    } else {
-                        if ((number - 1) * 5 <= index && (number * 5) > index) {
-                            console.log(index)
-                            return (<Vacancy
-                                key={item.id}
-                                nameEnterprise={item.company_name}
-                                logo={item.company_logo}
-                                namePosition={item.title}
-                                status={item.job_type || 'NOT FOUND'}
-                                cityName={item.candidate_required_location || 'WorldWide'
-                                }
-                                day={daysAgo(item.publication_date)}
-                            />);
+            {length.map((item, i) => {
+                if ((number - 1) * 5 <= i && (number * 5) > i){
+                    return (<Vacancy
+                        key={item.id}
+                        nameEnterprise={item.company_name}
+                        logo={item.company_logo}
+                        namePosition={item.title}
+                        status={item.job_type || 'NOT FOUND'}
+                        cityName={item.candidate_required_location || 'WorldWide'
                         }
-                    }
-                })}
+                        day={daysAgo(item.publication_date)}
+                    />);
+                }
+            })
+            }
         </div>
     );
 }
-function lengthArray(data){
+function lengthArray(data, status) {
     var cont = 0;
-    data &&
-    data.jobs.map((item, index) =>{
-        if (item.job_type === 'full_time') {
-            cont++
-        }
-    } )
-    return cont;
+    var a = []
+    if (status === true) {
+        data &&
+            data.jobs.map((item, index) => {
+                if (item.job_type === 'full_time') {
+                    a.push(item)
+                }
+            })
+    } else {
+        data &&
+            data.jobs.map((item, index) => {
+                a.push(item)
+            })
+    }
+    return a;
 }
 function daysAgo(data) {
     var info = String(data);
