09702f4e2984b17662aadfe0bfd9f6694755f9b8
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-03-06 23:51:14 +0100

    Updates

---
+++ precache-manifest.ca2cf1599f767445b3c84a59224da0e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bff0928913c860bd71f",
+    "url": "/github-jobs/static/css/main.9f0abea3.chunk.css"
+  },
+  {
+    "revision": "9bff0928913c860bd71f",
+    "url": "/github-jobs/static/js/main.9bff0928.chunk.js"
+  },
+  {
+    "revision": "56453f0a26f91e4139fc",
+    "url": "/github-jobs/static/js/1.56453f0a.chunk.js"
+  },
+  {
+    "revision": "4aa9ea75be5e2f7de90b",
+    "url": "/github-jobs/static/js/runtime~main.4aa9ea75.js"
+  },
+  {
+    "revision": "fc568113ad72433c2e3802588369dd2a",
+    "url": "/github-jobs/static/media/backgroundImg.fc568113.png"
+  },
+  {
+    "revision": "2b06d662417fdc29790bcc9b4164efe7",
+    "url": "/github-jobs/index.html"
+  }
+];
